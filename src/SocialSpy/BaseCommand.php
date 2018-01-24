4e314bdabed7f225d4e4473bcc34abdaefca2531
iZeaoGamer <jameshayward14@icloud.com>
2018-01-24 15:01:14 +0000

    Added files.

---
+++ src/SocialSpy/BaseCommand.php
@@ -0,0 +1,16 @@
+<?php
+namespace SocialSpy;
+use pocketmine\command\Command;
+use pocketmine\command\PluginIdentifiableCommand;
+use SocialSpy\Main;
+abstract class BaseCommand extends Command implements PluginIdentifiableCommand {
+    public $prefix = "§8[§aEP§8]§6 ";
+	private $plugin;
+	public function __construct(Main $plugin, $name, $desc = "", $usage, array $aliases = []){
+		parent::__construct($name, $desc, $usage, $aliases);
+		$this->plugin = $plugin;	
+       }
+	public function getPlugin(){
+		return $this->plugin;
+	}
+}
---
+++ src/SocialSpy/Commands/CommandSpy.php
@@ -0,0 +1,29 @@
+<?php
+namespace SocialSpy\Commands;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as Text;
+use pocketmine\Player;
+use SocialSpy\Main;
+use SocialSpy\BaseCommnd;
+class CommandSpy extends BaseCommand {
+	public function __construct(Main $plugin){
+        parent::__construct($plugin, "commandspy", "Spy on others commands!", "/commandspy", ['cmdspy']);
+        $this->setPermission("epFactions.commandspy");
+    }
+	public function execute(CommandSender $sender, $label, array $args){
+		if(!$this->testPermission($sender)){
+			$sender->sendMessage($this->prefix . "§cYou don't have permission for that!");
+			return true;
+		}
+		
+		if ($this->getPlugin()->hasCommandSpy($sender)) {
+			$sender->sendMessage($this->prefix . "You have disabled Command Spy");
+			$this->getPlugin()->removeCommandSpy($sender);
+			return true;
+		} else {
+			$sender->sendMessage($this->prefix . "You have enabled Command Spy");
+			$this->getPlugin()->enableCommandSpy($sender);
+			return true;
+		}
+	}
+}
