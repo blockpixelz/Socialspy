b2699bdf8a27c9489f5d1651ea5a0e796e07978c
iZeaoGamer <jameshayward14@icloud.com>
2018-02-05 16:40:04 +0000

    Attempt fix Crashes.

--- src/SocialSpy/BaseCommand.php
+++ src/SocialSpy/BaseCommand.php
@@ -4,13 +4,13 @@ use pocketmine\command\Command;
 use pocketmine\command\PluginIdentifiableCommand;
 use SocialSpy\Main;
 abstract class BaseCommand extends Command implements PluginIdentifiableCommand {
-    public $prefix = "§8[§aEP§8]§6 ";
+    public $prefix = "§8[§aVM§8]§6 ";
 	private $plugin;
 	public function __construct(Main $plugin, string $name, string $desc = "", string $usage, array $aliases = []){
 		parent::__construct($name, $desc, $usage, $aliases);
 		$this->plugin = $plugin;	
        }
-	public final function getPlugin(): BaseCommand{
+	public final function getPlugin(): Command{
 		return $this->plugin;
 	}
 }
